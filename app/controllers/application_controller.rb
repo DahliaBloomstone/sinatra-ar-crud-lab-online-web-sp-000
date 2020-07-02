763d4dae9b4c5de4d1fb663d550c3f6007cce332
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-02 21:18:07 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,51 +1,53 @@
 
-require_relative '../../config/environment'
+rrequire_relative '../../config/environment'
+class ApplicationController < Sinatra::Base	class ApplicationController < Sinatra::Base
+  configure do	  configure do
+    set :public_folder, 'public'	    set :public_folder, 'public'
+    set :views, 'app/views'	    set :views, 'app/views'
+  end	  end
 
-class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
-
-  get '/' do
-  end
-
-  get '/articles' do
+  get '/' do	  get '/articles' do
     @articles = Article.all
     erb :index
+  end	  end
+
+  get '/articles/new' do
+    erb :new
   end
 
-get 'articles/:id' do
-  @article = Article.find(params[:id])
-      erb :show
+  post '/articles' do
+    attrs = params
+    @article = Article.create(attrs)
+
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+    @article = Article.find_by(id: id)
+    attrs = params[:article]
+    @article.update(attrs)
+    redirect to "/articles/#{@article.id}"
+  end
+
+    delete '/articles/:id' do
+    id = params[:id]
+    Article.destroy(id)
+    redirect to '/articles'
     end
 
-get '/articles/new' do
-  erb :new
-end
-
-post '/articles' do
-  @article = Article.create(params[:article])
-  erb :show
-  end
-
-get '/articles/:id/edit' do
-  @article = Article.find(params[:id])
-  erb :edit
-end
-
-patch '/articles/:id' do
-  @article = Article.find(params[:id])
-   @article.update(params[:edit_article])
-   @article.save
-   erb :show
-end
-
-delete '/articles/:id/delete' do
-    @article = Article.find(params[:id])
-    @Article.destroy
-    erb :delete
-  end
-
-end
+end	end
